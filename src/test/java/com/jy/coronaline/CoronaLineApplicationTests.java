035c276841e040ce0a71cac9ec47f60906beabd8
hori_q@naver.com <hori_q@naver.com>
2022-02-10 15:41:23 +0900

    #1 - project setup
    project initialize

---
+++ src/test/java/com/jy/coronaline/CoronaLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jy.coronaline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
